37340fea58a3a306903ca592e9916362008c4473
alumno <alumno@alumno-desktop>
2015-03-10 11:20:59 +0100

    jeje

--- GitTest-1/src/package1/class1.java
+++ GitTest-1/src/package1/class1.java
@@ -2,7 +2,7 @@ package package1;
 
 public class class1 {
 	public static void main(String[] args) {
-		method1("Texto de salida numero 5");
+		method1("Texto de salida numero 666");
 	}
 	public static void method1(String str){
 		System.out.println(str);
