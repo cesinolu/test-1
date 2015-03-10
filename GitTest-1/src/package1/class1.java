2fb546184091a55ca4af9ff4ceb361e35f7233ef
alumno <alumno@alumno-desktop>
2015-03-10 10:29:19 +0100

    xd joder

--- GitTest-1/src/package1/class1.java
+++ GitTest-1/src/package1/class1.java
@@ -2,7 +2,7 @@ package package1;
 
 public class class1 {
 	public static void main(String[] args) {
-		method1("Texto de salida numero 33 :D");
+		method1("Texto de salida numero 33 :D ._.");
 	}
 	public static void method1(String str){
 		System.out.println(str);
